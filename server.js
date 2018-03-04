58a39609480a3c285b1e3c9b8461c1a75cc757fb
Daniel Adalaid Carranza <A01270898@itesm.mx>
2018-03-04 15:53:11 -0600

    fix to deploy

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-const express = require('express');
-var app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+var express  = require('express');
+var app      = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({'extended': 'true'}));
+app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 app.use(express.static('www'));
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function () {
-  console.log('Express server listen in port ' + app.get('port'));
+  console.log('Express server listening on port ' + app.get('port'));
 });
