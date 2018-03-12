71f4f4659de643b06e2c3989b7f45dc08ae3b4e8
Daniel Carranza <212637753@logon.ds.ge.com>
2018-03-12 10:11:03 -0600

    resolve some issues

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { CommonsProvider } from '../../providers/commons';
