19626154ee8c5aadde7802f645d3fe28853f4106
adal347 <adita0898@gmail.com>
2018-03-19 16:08:41 -0600

    Merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,12 +18,8 @@ import { CommonsProvider } from '../providers/commons';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-<<<<<<< HEAD
   rootPage: any = CuentasPagarPage;
-=======
-  rootPage: any = CuentasCobrarPage;
->>>>>>> feature/cuentas-cobrar
-
+  
   pages: Array<{title: string, component: any}>;
 
   constructor(public platform: Platform, public statusBar: StatusBar,
