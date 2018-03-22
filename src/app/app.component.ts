d6eea6803b2dbba8f3c79d0c54a202f5d1fa736a
Itzel <itzel.mg@hotmail.com>
2018-03-21 18:33:41 -0600

    IngresosEgresosNuevo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ import { CommonsProvider } from '../providers/commons';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = LoginPage;
+  rootPage: any = IngresosEgresosPage;
 
   pages: Array<{title: string, component: any}>;
 
