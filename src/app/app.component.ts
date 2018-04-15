cbcbf02d57a30b624041e699d7d37866564f7699
Daniel Adalaid Carranza <A01270898@itesm.mx>
2018-04-15 13:49:52 -0500

    change to Login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ import { CommonsProvider } from '../providers/commons';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = IngresosEgresosPage;
+  rootPage: any = LoginPage;
 
   pages: Array<{title: string, component: any}>;
 
